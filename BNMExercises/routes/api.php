3e77d38a2bd4fee5ceb239ae32c6d1a93df2a759
Amirul Farhan <farhan.kbs@gmail.com>
2025-06-24 12:54:33 +0800

    Initial commit

---
+++ BNMExercises/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->get('/dashboard', function (Request $request) {
+    return response()->json([
+        'message' => 'Welcome to your dashboard, ' . $request->user()->name
+    ]);
+});
